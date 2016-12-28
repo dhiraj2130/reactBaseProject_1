a12e14838ed0363d891b05a6d8e13fbe02411727
dhiraj_redhad <dhirajvit@yahoo.com>
2016-12-28 23:47:51 +1100

    form

--- reactApp/App.jsx
+++ reactApp/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+//https://www.tutorialspoint.com/reactjs/reactjs_component_life_cycle.htm
 class App extends React.Component {
-constructor(){
-    super();
+constructor(props){
+    super(props);
+
     this.state={
+        header:"this is app header set in App",
         data:
         [
             {
@@ -18,7 +20,47 @@ constructor(){
             }
         ]
     }
+    this.setStateHandler = this.setStateHandler.bind(this);
 }
+    componentWillMount() {
+        console.log('Component WILL MOUNT!')
+    }
+
+    componentDidMount() {
+        console.log('Component DID MOUNT!')
+    }
+
+    componentWillReceiveProps(newProps) {
+        console.log('Component WILL RECIEVE PROPS!')
+    }
+
+    shouldComponentUpdate(newProps, newState) {
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Component WILL UPDATE!');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Component DID UPDATE!')
+    }
+
+    componentWillUnmount() {
+        console.log('Component WILL UNMOUNT!')
+    }
+
+setStateHandler (){
+    var item = {
+        "id":100,
+        "name":"stateSetBar",
+        "age":"300"
+    };
+    var myArray = this.state.data;
+    myArray.push(item);
+    this.setState({data:myArray})
+};
+
     render() {
         var myStyle = {
             fontSize : 50,
@@ -28,17 +70,21 @@ constructor(){
         return (
             <div>
                 <h1 style ={myStyle} >Hello World!!!</h1>
-                <Header/>
-                <Content/>
+                <Header headerProps={ this.state.header }/>
+                <Content contentHeader={ this.state.header }/>
                 <table>
                     <tbody>
                     {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
                     </tbody>
                 </table>
+                <button onClick={this.setStateHandler}> Set State </button>
+
             </div>
         );
     }
 }
+
+
 class TableRow extends React.Component {
     render() {
         return (
@@ -52,19 +98,27 @@ class TableRow extends React.Component {
 }
 class Header extends React.Component{
     render(){
+        var myStyle ={
+            border:"solid red 2px"
+        }
         return (
-            <div>
-                <h1>header 1</h1>
+            <div  style={myStyle}>
+
+                <h1>{ this.props.headerProps }</h1>
             </div>
         )
     }
 }
 class Content extends React.Component{
+
     render(){
+        var myStyle ={
+            border:"solid green 2px"
+        }
         return(
-            <div>
+            <div style={myStyle}>
                 <h2>Content</h2>
-                <p> This is content text !</p>
+                 { this.props.contentHeader}
             </div>
         );
     }
