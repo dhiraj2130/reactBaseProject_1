03463c0c99f92f720064983b559635f16522e117
dhiraj_redhad <dhirajvit@yahoo.com>
2017-01-08 16:13:08 +1100

    testing upgraded

--- sampleProject_reactNoteTaker/src/test/components/Home.test.js
+++ sampleProject_reactNoteTaker/src/test/components/Home.test.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
-
-
-const Home = React.createClass({
-    render:function(){
-        return (
-             <h2 className="text-center">
-                 Search by Github account
-                 </h2>
-
-        )
-    }
-});
+import Home from '../../app/components/Home';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+// expect-jsx is used for better error message when test fails
+//https://egghead.io/lessons/react-testing-the-redux-store-multiple-actions?series=react-testing-cookbook
 describe('Home component',()=>{
+
+    function getRenderer(){
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<Home/>);
+    const actual = renderer.getRenderOutput();
+    return actual;
+}
     it('should return header',()=>{
-        const renderer = TestUtils.createRenderer();
-        renderer.render(<Home/>);
-        const output = renderer.getRenderOutput();
-        console.log(output);
+
+        const expected =<h2 className="text-center"> Search by Github account </h2>;
+        expect(getRenderer()).toIncludeJSX(expected);
+    });
+
+
+    it('should test the type of root object',()=>{
+
+    expect(getRenderer().type).toEqual('h2');
+    });
+
+    it('should test the class name object',()=>{
+
+    expect(getRenderer().props.className.includes('text-center')).toEqual(true);
+    });
+
+    it('should test the children',()=>{
+
+        expect(getRenderer().props.children.includes('text-center')).toEqual(true);
     });
 });
 
