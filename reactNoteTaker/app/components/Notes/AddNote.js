cfa749b87f4a0e4a7e9fb38c0ad0cabbd68cb17f
dhiraj_redhad <dhirajvit@yahoo.com>
2017-01-08 22:01:03 +1100

    test

---
+++ reactNoteTaker/app/components/Notes/AddNote.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dhiraj on 08/01/2017.
+ */
+var React=require('react');
+
+var AddNote = React.createClass({
+
+});
+
+module.exports=AddNote;
--- reactNoteTaker/app/components/Profile.js
+++ reactNoteTaker/app/components/Profile.js
@@ -21,12 +21,15 @@ var Profile = React.createClass({
     componentDidMount : function(){
       this.ref = new Firebase('https://github-note-taker.firebaseio.com/');
         var childRef = this.ref.child(this.props.params.username);
-        console.log("heeo "+ childRef);
+       // console.log("heeo "+ childRef);
         this.bindAsArray(childRef, 'notes');
     },
     componentWillUnmount : function(){
         this.unbind('notes');
     },
+    handleAddNote: function(newNote){
+        this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote);
+    }
     render:function(){
         return (
             <div className="row">
@@ -37,7 +40,10 @@ var Profile = React.createClass({
                     <Repos username={this.props.params.username} repos={this.state.repos}/>
                 </div>
                 <div className="col-md-4">
-                    <Notes username={this.props.params.username}  notes={this.state.notes}/>
+                    <Notes
+                        username={this.props.params.username}
+                        notes={this.state.notes}
+                        addNote={this.handleAddNote}/>
 
                 </div>
             </div>
